0bee3dca7329cab2b99803d49e187e8a17d2763d
Artemy Izmaylov <artemyizmaylov@gmail.com>
2021-10-27 15:23:12 +0300

    refactor: рефакторинг скриптов

--- scripts/form.js
+++ scripts/form.js
@@ -1,12 +1,11 @@
-// Находим форму в DOM
 let formElement = document.querySelector('.popup__form');
-// Находим поля формы в DOM
+
 let nameInput = formElement.querySelector('.popup__input_type_name')
 let aboutInput = formElement.querySelector('.popup__input_type_about')
-// Выберите элементы, которые появятся в полях ввода
+
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
-// Вставте элементы в поля ввода
+
 nameInput.value = profileName.textContent
 aboutInput.value = profileAbout.textContent
     
@@ -14,15 +13,13 @@ function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                                 // Так мы можем определить свою логику отправки.
                                                 // О том, как это делать, расскажем позже.
-    // Выберите элементы, куда должны быть вставлены значения полей
     profileName = document.querySelector('.profile__name');
     profileAbout = document.querySelector('.profile__about');
-    // Вставьте новые значения с помощью textContent
+
     profileName.textContent = nameInput.value;
     profileAbout.textContent = aboutInput.value;
-    // Закрываем попап
+
     popup.classList.remove('popup_opened');
 }
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+
 formElement.addEventListener('submit', formSubmitHandler);
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,7 +1,6 @@
-// Находим нужные кнопки
 let buttonEdit = document.querySelector('.button_type_edit');
 let buttonClose = document.querySelector('.button_type_close');
-// Находим попап
+
 let popup = document.querySelector('.popup');
 
 buttonEdit.addEventListener('click', function () {
