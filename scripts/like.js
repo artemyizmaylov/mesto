97bbad1ef57efa51595f5eb77fc3a1e19738a9e5
Artemy Izmaylov <artemyizmaylov@gmail.com>
2021-10-27 14:34:10 +0300

    fix: исправить пути до кнопок лайка

--- scripts/like.js
+++ scripts/like.js
@@ -1,9 +1,10 @@
 let likeButtons = document.querySelectorAll('.button_type_like');
 
-let active = "url('../../../images/like-active.svg')";
-let disable = "url('../../../images/like-disable.svg')";
+let active = 'url("./images/like-active.svg")';
+let disable = 'url("./images/like-disable.svg")';
 
 likeButtons.forEach(function (button) {
+    button.style.backgroundImage = disable;
     button.addEventListener('click', function () {
         if (button.style.backgroundImage === active) {
             button.style.backgroundImage = disable;
