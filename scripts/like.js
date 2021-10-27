f4006d6171f5cb217ddda8397d0e05f1af188171
Artemy Izmaylov <artemyizmaylov@gmail.com>
2021-10-27 13:53:44 +0300

    fix: оптимизировать фото, исправить путь до avatar

--- scripts/like.js
+++ scripts/like.js
@@ -1,7 +1,7 @@
 let likeButtons = document.querySelectorAll('.button_type_like');
 
-let active = 'url("../../../images/like-active.svg")';
-let disable = 'url("../../../images/like-disable.svg")';
+let active = "url('../../../images/like-active.svg')";
+let disable = "url('../../../images/like-disable.svg')";
 
 likeButtons.forEach(function (button) {
     button.addEventListener('click', function () {
